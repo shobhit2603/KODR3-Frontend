b43d3c73d9738f26361fb41b8ff1e585b67d5ad8
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-02-02 00:14:43 +0530

    Completed 20 questions in JS

---
+++ JavaScript/100-JS-Qs/02-Variables-&-DataTypes/q02-20.js
@@ -0,0 +1,42 @@
+// Declare a variable using let and log its value.
+let hello = "Hello World!";
+console.log(hello);
+
+// Create a constant to store the value of PI and log it.
+const PI = 3.14159;
+console.log(PI);
+
+// Reassign a value to a variable declared with let and log the result.
+let myName = "Jack";
+myName = "Shobhit";
+console.log(myName);
+
+// Check the type of null and log it.
+console.log(typeof null);
+
+// Create a variable with a number as a string(e.g., "25") and log its type.
+let age = "25";
+console.log(typeof age);
+
+// Use typeof to check the type of a boolean variable.
+let isAdult = true;
+console.log(typeof isAdult);
+
+// Create three variables of types string, number, and boolean, and log their values.
+let name = "Shobhit";
+let myAge = 23;
+let isDeveloper = true;
+console.log(name, myAge, isDeveloper);
+
+// Declare a variable without assigning a value.Log its type.
+let variable;
+console.log(typeof variable);
+
+// Create a variable with undefined and log its type.
+let ud = undefined;
+console.log(typeof ud);
+
+// Use const to create an array.Try reassigning the array and observe the error.
+const numbers = [1, 2, 3, 4, 5];
+numbers = [6, 7, 8, 9, 10];
+console.log(numbers);
