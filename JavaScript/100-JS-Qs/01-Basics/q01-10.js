b08942ec67c624db46b922565783590c8ef0efe7
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-02-02 00:02:21 +0530

    Did some changes

---
+++ JavaScript/100-JS-Qs/01-Basics/q01-10.js
@@ -0,0 +1,38 @@
+// Log your name and favorite hobby to the console.
+console.log("NAME: Shobhit Shrivastava");
+console.log("FAVORITE HOBBY: Solving Rubik's Cube");
+
+// Perform and log the result of 45 * 2 - 10.
+console.log(45 * 2 - 10);
+
+// Use console.log() to display the current year.
+console.log(new Date().getFullYear());
+
+// Create two variables for first and last name.Concatenate and log them.
+let firstName = "Shohbit";
+let lastName = "Shrivastava";
+console.log(firstName + " " + lastName);
+
+
+// Track the value of a variable by logging it before and after updating.
+let score = 50;
+console.log("Initial Score:", score);
+score += 25;
+console.log("Updated Score:", score);
+
+// Use console.error() to simulate an error message.
+console.error("This is a simulated error message.");
+
+// Log the square of the number 12 to the console.
+console.log(12 ** 2);
+
+// Print the type of a variable holding the value true.
+let isTrue = true;
+console.log(typeof isTrue);
+
+// Create a variable holding your age and log whether it’s greater than 18.
+let age = 23;
+console.log(age > 18);
+
+// Log the result of 100 / 0 and observe the output.
+console.log(100 / 0);
