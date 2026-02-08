22a905bf12562ddf8e648651599d718646c86160
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-02-08 19:00:16 +0530

    Started Focus Section

---
+++ ReactJS/1st-Project/src/components/ui/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { UserDataContext } from '../../context/UserContext';
+
+const Card = ({ cname, cdescription }) => {
+
+  return (
+
+
+    <div>
+      <h3 className="text-2xl font-bold mb-3 tracking-tighter">{cname}</h3>
+      <p>{cdescription}</p>
+    </div>
+
+  )
+}
+
+export default Card
